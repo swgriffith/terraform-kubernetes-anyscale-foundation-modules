093fd816af8311efae74d7db3f21c9242f6b43be
Hongchao Deng <hongchaodeng1@gmail.com>
2024-10-18 19:48:06 -0700

    gke existing cluster: remove var existing_vpc_id and use name to query it (#7)

--- examples/gcp/gke-existing_cluster/data.tf
+++ examples/gcp/gke-existing_cluster/data.tf
@@ -4,3 +4,7 @@ data "google_container_cluster" "anyscale" {
   name     = var.existing_gke_cluster_name
   location = var.existing_gke_cluster_region
 }
+
+data "google_compute_network" "existing_vpc" {
+  name = var.existing_vpc_name
+}
